76a1c5d4dadc35ecb92c0113f8e74f39f30f66c8
Paul Gamarra <paulgamarra.dev@gmail.com>
2023-11-11 17:26:11 -0300

    index (list tasks) and show (view details of the task) coded

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'tasks/index', to:"tasks#index"
+  get 'tasks/:id', to:"tasks#show", as: :task
+  get 'tasks/new', to:"tasks#new", as: :new_task
+  get 'tasks/create', to:"tasks#create"
+  get 'tasks/edit'
+  get 'tasks/update'
+  get 'tasks/delete'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
